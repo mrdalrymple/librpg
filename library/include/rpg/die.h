2cc9f88f7d8e44b16454c53bfde0800fa0b8a69e
David Warburton <david.warburton@gmail.com>
2018-06-11 12:02:14 -0700

    Forbid copy of Die

--- library/include/rpg/die.h
+++ library/include/rpg/die.h
@@ -9,6 +9,9 @@ class Die {
     Die(unsigned long sides);
     ~Die();
 
+    Die(const Die& o) = delete; //copying this class means the *pi needs to be copied
+    Die& operator=(const Die& o) = delete; //delete these until they are implemented.
+
     unsigned long roll();
 };
 
